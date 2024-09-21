24f7002e63083138b6723d0e9dc61a20d8cb4c1b
Ashwath Balaji <ashwathbalaji04@gmail.com>
2024-09-21 16:57:51 -0400

    commit

--- Chatbot.py
+++ Chatbot.py
@@ -1,10 +1,9 @@
 import speech_recognition as sr
 import google.generativeai as genai
-import webbrowser
 import os
 from dotenv import load_dotenv
 import pyttsx3
-import streamlit
+import streamlit as st
 
 
 
@@ -12,75 +11,129 @@ def userSpeech():
   
     recognizer = sr.Recognizer()
 
-    with sr.Microphone() as source:
-        print("Speak to Cody: ")
-        recognizer.adjust_for_ambient_noise(source)  # Adjust for ambient noise
+    try:
+        
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source)  # Adjust for ambient noise
 
-        try:
+        
             audio = recognizer.listen(source, timeout=5)  # Set the timeout to 5 seconds
             message = recognizer.recognize_google(audio)
-            #print(f"You said: {message}")
+            message = message.lower()
             return message
-          
-        # Catch the timeout exception   
-        except sr.WaitTimeoutError: 
-            print("No speech detected. Please try again.")
-            return None
+            
+            # Catch the timeout exception   
+    except sr.WaitTimeoutError: 
+        print("No speech detected. Please try again.")
+        return None
 
-        except sr.UnknownValueError:
-            print("Sorry, I couldn't understand what you said.")
-            return None
-          
-        except sr.RequestError:
-            print("Speech recognition service unavailable.")
-            return None
+    except sr.UnknownValueError:
+        print("Sorry, I couldn't understand what you said.")
+        return None
+            
+    except sr.RequestError:
+        print("Speech recognition service unavailable.")
+        return None
 
+
+
+def joinWordsInOutput(output):
+
+    arr = []
+    for word in output:
+        arr.append(word.text)
+    response = " ".join(arr)
+    
+    return response
+
+
+
+# Set up the model
+def generationConfig():
+    
+    generation_config = {
+        "temperature": 1, 
+        "top_p": 0.95, 
+        "top_k": 0, 
+        "max_output_tokens": 8192}
+    
+    return generation_config
+
+
+
+# Sets the settings to block explicit content
+def safetySettings():
+    
+    safety_settings = [ 
+        {"category": "HARM_CATEGORY_HARASSMENT", 
+         "threshold": "BLOCK_MEDIUM_AND_ABOVE"}, 
+        
+        {"category": "HARM_CATEGORY_HATE_SPEECH", 
+         "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+        
+        {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", 
+         "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+        
+        {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", 
+         "threshold": "BLOCK_MEDIUM_AND_ABOVE"}]
+    
+    return safety_settings
+
+    
+
+def genAiModel(chat, apiKey):
+
+    try:
+        genai.configure(api_key = apiKey) 
+        model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest", generation_config = generationConfig(), safety_settings = safetySettings())
+        convo = model.start_chat(history=[])
+        output = convo.send_message(chat, stream = True)
+        
+        if "search" and "local" and "file" in joinWordsInOutput(output):
+            st.write(LocalFileCalling())
+            
+        return joinWordsInOutput(output)
+    
+
+    except Exception as e:
+        print(f"An error occurred {e}")
+        return None
+        
 
 
 def speakToCody():
-
-    model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest", generation_config=generation_config, safety_settings=safety_settings)
-
-    convo = model.start_chat(history=[])
-    
     
     while True:
        
-      userMessage = userSpeech()
-      
-      if userMessage:
+        userMessage = userSpeech()
         
-        if "Google" in userMessage:
-          webbrowser.open('http://www.google.com')
-        
-        
-        if any(statement in userMessage.lower() for statement in listOfExitWords()):
-          convo.send_message(userMessage) 
-          print("Cody: " + convo.last.text)
-          callTts(convo.last.text)
-          return "Success"
-          break
-        
+        if "search" and "local" and "file" in userMessage:
+            LocalFileCalling()
           
-        convo.send_message(userMessage) 
-        print("Cody: " + convo.last.text)
-        callTts(convo.last.text)
+      
+        if userMessage:
         
-
-      else:
+            if any(statement in userMessage.lower() for statement in listOfExitWords()):
+                convo.send_message(userMessage) 
+                print("Cody: " + convo.last.text)
+                callTts(convo.last.text)
+                return "Success"
+                break
+          
+            convo.send_message(userMessage) 
+            print("Cody: " + convo.last.text)
+            callTts(convo.last.text)
         
-        print("Cody: No valid input received :( ")
-        callTts("No valid input received :( ") 
+        
+        else:
 
-
-
-def listOfExitWords():
-  
-    exitStatements = ["bye", "goodbye", "see ya", "that's it for today"]
-    return exitStatements
+            print("Cody: No valid input received :( ")
+            callTts("No valid input received :( ") 
     
 
+
 def callTts(message):
+    
     tts = pyttsx3.init()
     # tts.setProperty('rate', 75)
     voice = tts.getProperty("voices")
@@ -90,44 +143,78 @@ def callTts(message):
 
 
 
-def getApiKey():
-  
+def LocalFileCalling():
+    
+    current_directory = Path.cwd()
+    print("Current Directory:", current_directory)
+    print("\n")
+    #the root tells us the current directory, dirs will list the directories in this directory, and the files will list the files in the directory
+    file_found = False #keeps track of if file is found or not
+    st.write("Caps matter, type the name and the filetype ex:(bot.py)")
+    name = input("Enter filename): ",) # Add chatinput feature so that user can type that filename within streamlit
+    
+    for root, dirs, files in os.walk(f"{current_directory}"):
+        
+        for files in files:
+            
+            if files.endswith(name):
+                
+                return "FOUND THE FILE: " + files
+                file_found = True
+                
+    if file_found == False:
+        return "File not found"
+        
+
+
+def deployToSt(APIKEY):
+    
+    st.set_page_config(page_title = "Project Codex")
+    st.header("Welcome to Project Codex!")
+
+    # b1 = st.button("Click here to type to Cody")
+    # b2 = st.button("Click here to speak to Cody")
+
+    # if b1:
+
+    if "History" not in st.session_state:
+        st.session_state["History"] = []
+        
+
+    chat = st.chat_input("Type to Cody: ")
+
+    if chat:
+        st.write(f"You: {chat}")
+        answer = genAiModel(chat, APIKEY)
+        
+        if answer:
+            storeChat(chat, answer)
+
+
+
+def storeChat(chat, answer):
+    
+    st.session_state["History"].append(("You", chat))
+    st.session_state["History"].append(("Cody", answer))
+    st.write(f"Cody: {answer}")
+
+    st.subheader("Chat History ")
+
+    for inp, out in st.session_state["History"]:
+        st.write(f"{inp}: {out}")
+
+
+
+def main():
+    
+    os.system("pip install -r requirements.txt") 
+    print("\n Downloaded the required directories \n")
     load_dotenv()
     APIKEY = os.getenv("APIKEY")
-    return APIKEY
+    APIKEY2 = os.getenv("APIKEY2") # API key for weather api
+    deployToSt(APIKEY) 
 
 
 
 if __name__ == "__main__":
-  
-    
-    genai.configure(api_key=getApiKey()) # REMEMBER TO MAKE A ENCRYPTED FILE TO SECURE API KEY
-    
-    # Set up the model
-    generation_config = {
-      "temperature": 1,
-      "top_p": 0.95,
-      "top_k": 0,
-      "max_output_tokens": 8192,
-    }
-
-    safety_settings = [
-      {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-      },
-      {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-      },
-      {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-      },
-      {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-      },
-    ]
-    
-    print(speakToCody())
+    main()
